ace5ca8a8eeebf629349b1cefbb39689332533a9
Lynn 'Cyrin' Conway <lynncyrin@gmail.com>
2014-07-11 02:03:57 -0700

    point to our index page

--- web-app/main.py
+++ web-app/main.py
@@ -1,10 +1,12 @@
-from flask import Flask
+import flask
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 @app.route('/')
-def index(): 
-    return 'working'
+def index():
+    page_desc = ''
+    page_title = ''
+    return flask.render_template('index.html', page_title=page_title, page_desc=page_desc)
 
 if __name__ == '__main__':
     app.config['DEBUG'] = True
